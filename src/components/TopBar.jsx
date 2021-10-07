1298ad5170c95983849bdc2e4aeaed5d88d81d0b
Vanderllee <vanderllee@gmail.com>
2021-10-07 16:31:32 -0300

    adiciona fonticons

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,42 +1,57 @@
 /* eslint-disable @next/next/no-img-element */
-
+import { 
+    faFacebookSquare, 
+    faInstagramSquare, 
+    faPinterestSquare, 
+    faTwitterSquare
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import Topbar from '../styles/Topbar.module.css';
 
-import Image from "next/image";
-
-import { dataPost } from '../data/postData';
-import { useState } from 'react';
-
-
 export default function TopBar() {
 
-    const [post] = useState(dataPost);
 
 
     return (
         <div className={Topbar.top}>
+
+
             <div className= {Topbar.topLeft} >
                 <a
                     href="https://facebook.com/pages/E.M.E.F%20Engenheiro%20Jos%C3%A9%20Amadei/114176958598965/"
                     target="_blank"
                     rel="noreferrer"
                 >
-                    <i className={`${Topbar.topIcon} fab fa-facebook-square`}></i>  
+                    <i className={`${Topbar.topIcon}`}>
+
+                        <FontAwesomeIcon icon={faFacebookSquare}/>    
+                    
+                    </i>  
                 </a>
 
-                <i className= {`${Topbar.topIcon} topIcon fab fa-twitter-square`} ></i>
-                <i className={`${Topbar.topIcon} topIcon fab fa-pinterest-square`} ></i>
+                <i className= {`${Topbar.topIcon}`} >
+                    <FontAwesomeIcon icon={faTwitterSquare}/> 
+                </i>
+                <i className={`${Topbar.topIcon} `} >
+
+                    <FontAwesomeIcon icon={faPinterestSquare}/> 
+                </i>
                 <a
                     href="https://instagram.com/vanderllee/"
                     target="_blank"
                     rel="noreferrer"
                 >
-                    <i className= {`${Topbar.topIcon} fab fa-instagram-square`} ></i>
+                    <i className= {`${Topbar.topIcon} `} >
+                        <FontAwesomeIcon icon={faInstagramSquare}/> 
+                    </i>
                 </a>
             </div>
+
+
+
             <div className={Topbar.topCenter}>
-                <ul className="topList">
+                <ul className={Topbar.topList}>
                     <li className={Topbar.topIcon}>
                         HOME
                     </li>
@@ -54,36 +69,14 @@ export default function TopBar() {
                         className={Topbar.topIcon}
                     >
 
-                   
-
                     </li>
                 </ul>
+
             </div>
-            <div className={Topbar.topRight}>
-
-                    
-                    {
-                        post.map(postItem =>  (
-
-                            <div key={postItem.id}>
-                            <Image
-                                width={100}
-                                height={100}
-                                src={postItem.photo}
-                                alt={''}
-                                objectFit="cover"
-                            />
-                            </div>
-
-                        ))
-                        
-                        
-                    }
                 
-
-
-                <i className= {`${Topbar.topSearchIcon} fas fa-search `}></i>
-            </div>
+        
+            
+               
         </div>
     )
 }

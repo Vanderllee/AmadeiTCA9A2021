f7c810376e037be897a3b67c47d11a1f789a7ff0
Vanderllee <vanderllee@gmail.com>
2021-10-08 21:01:41 -0300

    adiciona single page

--- src/components/Singlepage.jsx
+++ src/components/Singlepage.jsx
@@ -1,11 +1,50 @@
+/* eslint-disable @next/next/no-img-element */
 
+import styles from '../styles/Singlepage.module.css'
 
 export default function Singlepage(props) {
     return (
-        <div>
+            
+        <div className="singlepost">
+        <div className="singlePostWraper">
+            
+               
+                    <img src={props.post.imageURL} 
+                        alt="Minha imagem" 
+                        className="singlePostImg" 
+                    />
+
+            
+
+            <div className="singlePostInfo">
+                <span className="singlePostAuthor">
+                    Author: 
+                        <Link to={ ` /?user=${ post.username } ` } className="link">
+                            <b>{post.username}</b>
+                        </Link>
+                </span>
+
+                <span className="singlePostDate">
+                    { new Date(post.createdAt).toDateString() }
+                </span>
+            </div>
+
             {
-                props.post.title
+                updateMode 
+                ? (<textarea 
+                    className="singlePostDescInput" 
+                    value={ desc }
+                    onChange={(e) => setDesc(e.target.value)} 
+                />)
+                : (<p className="singlePostDesc"> { desc } </p>)
+            }
+
+           { updateMode 
+            && (<button className="singlePostButton"onClick={ handleUpdate }>Atualizar</button>)
             }
         </div>
+    </div>
+
+       
     )
 }

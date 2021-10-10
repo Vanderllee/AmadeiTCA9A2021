de60514d524734048b4714f040edb64817bf9b4c
Vanderllee <vanderllee@gmail.com>
2021-10-10 13:39:36 -0300

    adiciona conteudo

--- src/data/postData.js
+++ src/data/postData.js
@@ -1,35 +1,73 @@
 export const dataPost = [
     {
         id: "1",
-        title: "Como diminuir o consumo",
-        username: "José Vanderli",
+        title: "O que é consumismo ?",
+        username: "Luiz",
         date: '12/10/2021',
-        text: 'A importância de diminuir maus habitos de consumo...',
+        text: 'Consumismo é o consumo exagerado de qualquer coisa, sendo ela algo necessário como alimentos ou algo não necessário como produtos baratos que dão a falsa promessa de agilizar o dia a dia, mas que não passam de futilidades.',
         imageURL: "https://www.iberdrola.com/wcorp/gc/prod/pt_BR/comunicacion/reducir_consumo_plastico_mult_1_res/ConsejosPlastico_746x419_POR.jpg",
     },
 
     {
         id: "2",
-        title: "Devemos diminuir o consumo",
-        username: "André Silva",
-        text: 'A importância de diminuir maus habitos de consumo...',
+        title: " Origem do consumismo",
+        username: "Danilo",
+        text: `
+        Pode-se dizer que o consumismo não possui uma origem certa, desde a idade da pedra o ser humano sempre consumiu, seja isto por motivos de sobrevivência, higiene ou até mesmo bem estar. 
+        Com o passar dos séculos a sociedade foi evoluindo e desenvolvendo melhores práticas de sobrevivência, fazendo com que a população aumentasse, gerando assim a necessidade do aumento na produção de recursos. A resposta dessa necessidade foi a revolução industrial.                                                                                                                                    
+        A Revolução industrial começou a partir do século XVIII, seu principal objetivo foi fazer com que os produtos que antes eram feitos a mão, por artesãos, fossem agora produzidos em grande escala por máquinas. 
+        Isso possibilitou que a disponibilização dos produtos fosse muito maior e por consequência o consumo.  Vivemos em uma sociedade consumista desde sempre mas, foi a partir da revolução industrial que realmente começamos a consumir em alta escala e a um ponto em que realmente danifica-se o meio ambiente. 
+        
+        `,
         date: '12/10/2021',
         imageURL: "https://docelimao.com.br/site/images/cache/288c38ba0861a8308fb5e8f4b66b26f2_w640_h360_cp_sc.jpg",
     },
 
     {
         id: "3",
-        title: "Cosumismo é um problema grave",
-        username: "Pedro Alves",
-        text: 'A importância de diminuir maus habitos de consumo...',
+        title: "Por que consumimos ?",
+        username: "Lucas",
+        text: `
+        O nosso consumo é sempre por alguma necessidade. Já o consumo exagerado pode começar de várias formas, como por uma propaganda te influenciando a comprar um produto “milagroso” que você não pode perder.
+        Outra causa comum é a de padrões sociais, as pessoas tendem a comprar algo que seja tendência, não por o gostarem, mas por medo de julgamento e para se enturmar ou pior ainda, para exibirem sua riqueza com roupas e carros de marcas caras. Com a Infelicidade pessoal a tentativa de suprir seus problemas consumindo compulsivamente é um perigo. Além dos gastos financeiros que esse comportamento leva, existe a chance de se tornar um vício. Problemas psicológicos como depressão e ansiedade também são gatilhos para o consumo em excesso. 
+       
+        
+        `,
         date: '12/10/2021',
         imageURL: "https://www.masottiinvestimentos.com.br/upload/blogs/5-dicas-para-reduzir-o-consumo-de-energia-eletrica-92-042240.png",
     },
 
     {
         id: "4",
+        title: "Qual a diferença entre consumo e consumismo?",
+        username: "Danilo",
+        date: '12/10/2021',
+        text: `
+        Consumo e consumismo são conceitos diferentes, consumo nada mais é do que o ato de suprir as suas necessidades, ou seja, tudo que esteja ligado a sua sobrevivência, como água e comida. Já o consumismo rompe essas necessidades, adquirindo algo que não precisa, estando associado ao gasto de produto sem utilidade imediata, os denominados supérfluos. 
+        A grande diferença entre consumo e consumismo está na motivação que impulsiona a compra. O consumo refere-se à apropriação de algo com o objetivo de tratar uma necessidade relacionada à sobrevivência. Já o consumismo refere-se a um comportamento de consumo que extrapola a necessidade.
+        
+        `,
+        imageURL: "https://www.premix.com.br/blog/wp-content/uploads/2020/03/Infografico-Dia-Agua.jpg",
+    },
+    {
+        id: "5",
+        title: "Quais os problemas que o consumismo traz ?",
+        username: "João",
+        date: '12/10/2021',
+        text: `
+        
+        Os problemas que o consumismo traz já são extremamente evidentes em nossa sociedade. Aquecimento global, efeito estufa, emissão de gases poluentes, desflorestação todos eles estão relacionados de certa forma ao consumo inapropriado.	
+        Devido a industrialização e o pensamento capitalista de quanto mais dinheiro melhor, as empresas começaram a consumir o máximo de matéria prima do jeito menos eco-friendly  possível para a fabricação de produtos. Assim os danos para o meio ambiente são imensos, podendo acabar com o habitat de certas espécies e até levando à extinção, erosão e intensificação do efeito estufa devido a perda da natureza que absorve o gás carbônico. Outro problema que o consumismo traz é que com o excesso de consumo, vem como consequência o excesso de lixo acumulado.
+        Quero pôr ênfase de que não estou difamando o capitalismo nem o glorificando, até porque essa não é a intenção deste texto. Só estou dando uma crítica ao nosso atual sistema econômico para a reflexão do que nós como sociedade podemos mudar para o melhor desempenho do país e para preservação de sua fauna, flora e meio ambiente. 
+             Uma pergunta muito comum é se o consumismo é uma doença, e a resposta é que o consumo em excesso é sim considerada uma doença psicológica em certos casos que se chama Oneomania. 
+        
+        `,
+        imageURL: "https://www.premix.com.br/blog/wp-content/uploads/2020/03/Infografico-Dia-Agua.jpg",
+    },
+    {
+        id: "6",
         title: "Diminua consumo ",
-        username: "Carlos Eduardo",
+        username: "José Vanderli",
         date: '12/10/2021',
         text: 'A importância de diminuir maus habitos de consumo...',
         imageURL: "https://www.premix.com.br/blog/wp-content/uploads/2020/03/Infografico-Dia-Agua.jpg",
